8ac68f9255e968f20af5ad7cdaa37a4657d0bc99
Unknown <irenefm_3@hotmail.com>
2019-09-17 22:20:49 +0200

    definir item pokemon

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -8,7 +8,10 @@
 import scrapy
 
 
-class TutorialItem(scrapy.Item):
+class PokemonItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    id = scrapy.Field()
+    name = scrapy.Field()
+    height = scrapy.Field()
+    weight = scrapy.Field()
+    base_experience = scrapy.Field()
